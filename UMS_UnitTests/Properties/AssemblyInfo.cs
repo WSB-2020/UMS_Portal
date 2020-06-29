19d4f390a5a7a07a37216465ed5d4b9801981bbf
Krzysztof Krzepicki <krzysztof.krzepicki@gmail.com>
2020-06-29 11:13:57 +0200

    ok

---
+++ UMS_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UMS_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UMS_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8c6388e-09ec-4ef5-a244-7436aecedbb7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UMS_UnitTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UMS_Portal.DAL;
+using UMS_Portal.Services;
+
+namespace UMS_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MainDataAccessService das = new MainDataAccessService( new ApplicationDbContext());
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+
+            var x = await das.GetUserList();
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
